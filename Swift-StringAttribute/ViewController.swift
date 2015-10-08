91a50d194f2684defe9cf8500e35e9bb064083a7
YouXianMing <705786299@qq.com>
2015-10-08 22:02:55 +0800

    Initial Commit

---
+++ Swift-StringAttribute/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift-StringAttribute
+//
+//  Created by YouXianMing on 15/10/8.
+//  Copyright © 2015年 YouXianMing. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
